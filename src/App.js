9a42baaff49b9db055fc786e5115c2fa092da80a
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:09:10 -0500

    feat: add Auto Review article

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import BiologyToday from './components/biologyToday'
+import AutoReview from './components/autoReview'
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         </p>
       </header>
       <BiologyToday/>
+      <AutoReview/>
     </div>
   );
 }
